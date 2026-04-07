a043839c6a87875bcf22fe6bf68827575c2603b5
pramodvishwakarma <pramod00106@gmail.com>
2026-04-07 14:17:31 +0530

    perf: use shallow clone in build.sh for faster Vercel builds

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Install Flutter (stable)
-git clone https://github.com/flutter/flutter.git -b stable
+# Install Flutter (stable) with shallow clone for speed
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
 export PATH="$PWD/flutter/bin:$PATH"
 
 flutter config --enable-web
