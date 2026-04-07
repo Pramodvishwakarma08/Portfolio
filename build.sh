5fb893bbfd14e20872ab5d158232da51a663063a
pramodvishwakarma <pramod00106@gmail.com>
2026-04-07 15:55:10 +0530

    ci(vercel): update build config and add SPA

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash 
 set -e 
  
-# Install Flutter 
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 
+# Install Flutter if it doesn't exist
+if [ ! -d "flutter" ]; then
+  echo "Cloning Flutter..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 
+fi
+
 export PATH="$PWD/flutter/bin:$PATH" 
  
 flutter config --enable-web 
